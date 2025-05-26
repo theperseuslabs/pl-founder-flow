e43b0c2219150f30723be2acd3861eeee67bd0c3
ak <kothari.atit@gmail.com>
2025-05-26 11:04:14 -0700

    variable update

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaA7KcEfchQBh3vyu09HaKbgSKPhT629U",
-  authDomain: "easymarketingautomation.firebaseapp.com",
-  projectId: "easymarketingautomation",
-  storageBucket: "easymarketingautomation.firebasestorage.app",
-  messagingSenderId: "348263426054",
-  appId: "1:348263426054:web:2da1f67300d44f0e157b87",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase for the client side only
